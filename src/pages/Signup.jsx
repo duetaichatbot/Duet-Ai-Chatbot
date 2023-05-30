52df2099f87861b508ed1c9418248010ffbe4ec4
ahmedashar <ahmedashar12345@gmail.com>
2023-05-30 14:43:58 +0500

    Signup-UI

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -2,26 +2,28 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from 'react-router-dom';
 import { registerUser } from "../redux/userSlice";
+import '../../src/index.css'
+import chatbot from '../assets/chatbot3.gif';
 
 const Signup = () => {
 
-    const navigate = useNavigate();
-    const [error, setError] = useState('');
-    const [registerFields, setRegisterFields] = useState({
-        name: "",
-        email: "",
-        password: "",
-        cpassword: "",
-        tc: false,  
-    });
+  const navigate = useNavigate();
+  const [error, setError] = useState('');
+  const [registerFields, setRegisterFields] = useState({
+    name: "",
+    email: "",
+    password: "",
+    cpassword: "",
+    tc: false,
+  });
   const dispatch = useDispatch();
 
 
   const handleRegisterField = (e) => {
     if (e.target.name === 'tc') {
-        setRegisterFields({...registerFields, [e.target.name]: e.target.checked})
-    }else{
-        setRegisterFields({...registerFields, [e.target.name]: e.target.value})
+      setRegisterFields({ ...registerFields, [e.target.name]: e.target.checked })
+    } else {
+      setRegisterFields({ ...registerFields, [e.target.name]: e.target.value })
     }
   }
 
@@ -29,59 +31,86 @@ const Signup = () => {
   const handleRegister = (e) => {
     e.preventDefault();
     dispatch(registerUser(registerFields))
-    .then(res => {
-      console.log(res, 'this is created user response...');
-      if (res.payload.token) {
-        alert('login successfully!');
-      }else{
+      .then(res => {
+        console.log(res, 'this is created user response...');
+        if (res.payload.token) {
+          alert('login successfully!');
+        } else {
           setError(res.payload.message)
-      }
-    })
+        }
+      })
   };
 
   return (
-    <div>
-      <form>
-        <input
-          type="text"
-          name="name"
-          value={registerFields.name}
-           onChange={handleRegisterField}
-          placeholder="Enter Name"
-        />
-        <br />
-        <input
-          type="email"
-          name="email"
-          value={registerFields.email}
-           onChange={handleRegisterField}
-          placeholder="Enter Email"
-        />
-        <br />
-        <input
-          type="password"
-          name="password"
-          value={registerFields.password}
-           onChange={handleRegisterField}
-          placeholder="Enter Password"
-        />
-        <br />
-        <input
-          type="password"
-          name="cpassword"
-          value={registerFields.cpassword}
-           onChange={handleRegisterField}
-          placeholder="Confirm Password"
-        />
-        <br />
-        <input type="checkbox" name="tc" onChange={handleRegisterField} />
-        <label htmlFor="">Agree term & Condition</label>
-        <br />
-        <button onClick={handleRegister}>Submit</button>
-      </form>
-      <div>
-        <h3> {error}</h3>
+    <div className="signup_container">
+      <div className="item_1">
+        <div className="form_container">
+          <div className="signup_heading">
+          <span className="signup_span">Signup please</span>
+          </div>
+          {/* form-inputs */}
+          <form className="signup_form">
+            <input
+              className="name_input  signup_input"
+              type="text"
+              name="name"
+              value={registerFields.name}
+              onChange={handleRegisterField}
+              placeholder="Enter Name"
+            />
+            <br />
+            <input
+            className="email_input signup_input"
+              type="email"
+              name="email"
+              value={registerFields.email}
+              onChange={handleRegisterField}
+              placeholder="Enter Email"
+            />
+            <br />
+            <input
+             className="password_input signup_input"
+              type="password"
+              name="password"
+              value={registerFields.password}
+              onChange={handleRegisterField}
+              placeholder="Enter Password"
+            />
+            <br />
+            <input
+             className="cpassword_input signup_input"
+              type="password"
+              name="cpassword"
+              value={registerFields.cpassword}
+              onChange={handleRegisterField}
+              placeholder="Confirm Password"
+            />
+            <br />
+            <br />
+            <input className="checkbox_input" type="checkbox" name="tc" onChange={handleRegisterField} />
+            <label className="checkbox_label" htmlFor="">Agree term & Condition</label>
+            <br />
+
+            <div className="signup_btn_div">
+            <button className="signup_btn" onClick={handleRegister}>SIGN UP</button>
+            </div>
+          </form>
+
+          <div>
+            <h3 className="error_msg"> {error}</h3>
+          </div>
+        </div>
       </div>
+
+      <div className="item_2">
+        <h3>WELCOME!</h3>
+        <p>Enter your details and start your journey with us</p>
+        <img src={chatbot} alt="" />
+
+
+      </div>
+
+
     </div>
   );
 };
