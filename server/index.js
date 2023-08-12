5a1c9b1bc5e15166061953cb9aa0762990332ed0
Malik786-git <malikmuhammadjahangir786@gmail.com>
2023-08-13 02:52:16 +0500

    initiate dialog flow on server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,8 @@ import express from 'express';
 import cors from 'cors';
 import { connectDb } from './config/connectdb.js';
 import userRouter from './routes/userRoutes.js';
-import path from 'path'
+import path from 'path';
+import morgan from 'morgan';
 
 dotenv.config();
 
@@ -16,14 +17,108 @@ const DATABASE_URL = process.env.DATABASE_URL;
 
 
 app.use(cors());
+app.use(morgan("dev"));
 connectDb(DATABASE_URL);
 
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 
+
+
+// //////////////////////////////////////////////
+// ///////// Dialogflow Integratino     ////////
+// request object 
+const webhookReq = {
+    "responseId": "response-id",
+    "session": "projects/project-id/agent/sessions/session-id",
+    "queryResult": {
+      "queryText": "End-user expression",
+      "parameters": {
+        "param-name": "param-value"
+      },
+      "allRequiredParamsPresent": true,
+      "fulfillmentText": "Response configured for matched intent",
+      "fulfillmentMessages": [
+        {
+          "text": {
+            "text": [
+              "Response configured for matched intent"
+            ]
+          }
+        }
+      ],
+      "outputContexts": [
+        {
+          "name": "projects/project-id/agent/sessions/session-id/contexts/context-name",
+          "lifespanCount": 5,
+          "parameters": {
+            "param-name": "param-value"
+          }
+        }
+      ],
+      "intent": {
+        "name": "projects/project-id/agent/intents/intent-id",
+        "displayName": "matched-intent-name"
+      },
+      "intentDetectionConfidence": 1,
+      "diagnosticInfo": {},
+      "languageCode": "en"
+    },
+    "originalDetectIntentRequest": {}
+}
+
+  
+app.post("/webhook", async (req, res) => {
+   try {
+        const body = req.body;
+        const intentName  = body.queryResult.intent.displayName;
+        console.log(intentName, 'name');
+        const params = body.queryResult.parameters;
+        if (intentName === "placeOrder") {
+            console.log("collected params", params);
+
+            let responseText = `you said ${params.qty} ${params.pizzaSize} ${params.pizzaFlavour} pizza,  your pizza is on the way, this is came from webhook server`;
+
+            res.send({
+                "fulfillmentMessages": [
+                  {
+                    "text": {
+                      "text": [
+                        responseText,
+                        "alternate res from webhook server.."
+                      ]
+                    }
+                  }
+                ]
+              })
+        }else{
+            res.send({
+                "fulfillmentMessages": [
+                  {
+                    "text": {
+                      "text": [
+                        "sorry don't answer for intent"
+                      ]
+                    }
+                  }
+                ]
+              })
+        }
+   } catch (error) {
+        console.log(error);
+        res.status(500).send({
+            message: "Internal Server Error"
+        })
+   }
+});
+
+
+// //////////////////////////////////////////////
+// //////////////////////////////////////////////
 // Load Routes
 app.use("/api/user/", userRouter)
+app.use("/test", (req, res)=> res.send("test ngrok server"))
 
 app.listen(port, ()=>{
     console.log(`Server listening at localhost:${port}`);
